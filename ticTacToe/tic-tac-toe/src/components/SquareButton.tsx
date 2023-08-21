e50733ad492d895a65faf53edd6dc186dd75d5c6
Gadi <guppiefriedman@gmail.com>
2023-08-21 10:13:50 +0200

    Container for grid of buttons added and deployed

---
+++ ticTacToe/tic-tac-toe/src/components/SquareButton.tsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+function SquareButton() {
+  return (
+    <div>
+      <button className="testButton"></button>
+    </div>
+  );
+}
+
+export default SquareButton;
